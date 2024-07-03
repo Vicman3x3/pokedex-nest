7de299d1229ec5cd1e63052f77efe98928e75685
vicman3x3 <victorm.cov@gmail.com>
2024-07-02 22:38:03 -0600

    Manejo de errores para respuestas del servicio

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Model } from 'mongoose';
@@ -15,8 +15,16 @@ export class PokemonService {
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
-    const pokemon = await this.pokemonModel.create(createPokemonDto);
-    return pokemon;
+    try{
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch(err){
+      if(err.code === 11000){
+        throw new BadRequestException(`El Puchamon ${JSON.stringify(err.keyValue)} ya existe en base de datos`)
+      }
+      console.log(err);
+      throw new InternalServerErrorException(`No se puede crear el Pokemon - revisar los logs`)
+    }
   }
 
   findAll() {
